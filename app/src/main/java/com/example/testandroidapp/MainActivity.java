3162033b24061895f0f2c1deec70832942861f82
Jacob Greenway <jagreenway12@gmail.com>
2019-02-24 01:11:44 -0600

    add location based map start

--- app/src/main/java/com/example/testandroidapp/MainActivity.java
+++ app/src/main/java/com/example/testandroidapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.testandroidapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.support.design.internal.BottomNavigationItemView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,6 +31,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback, ARViewFrag.OnFragmentInteractionListener {
 
@@ -32,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private FirebaseFirestore db;
     private GoogleMap map;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private List<ScanResult> scanResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +67,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Add AR fragment
         this.arFragment = new ARViewFrag();
+
+        // Set location client
+        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         // Render map fragment
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -95,17 +111,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Get router locations from firebase and add pins to map
         CollectionReference routers = db.collection("routers");
-        routers.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot snap) {
-                for (QueryDocumentSnapshot router : snap) {
-                    GeoPoint location = (GeoPoint) router.get("location");
-                    LatLng marker = new LatLng(location.getLatitude(), location.getLongitude());
-                    map.addMarker(new MarkerOptions().position(marker).title((String) router.get("ssid")));
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15));
-                }
+        routers.get().addOnSuccessListener(snap -> {
+            for (QueryDocumentSnapshot router : snap) {
+                GeoPoint location = (GeoPoint) router.get("location");
+                LatLng marker = new LatLng(location.getLatitude(), location.getLongitude());
+                map.addMarker(new MarkerOptions().position(marker).title((String) router.get("ssid")));
             }
         });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        this.fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        LatLng marker = new LatLng(location.getLatitude(), location.getLongitude());
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15));
+                    }
+                });
+    }
+
+    public List<ScanResult> getList(){
+        return this.scanResults;
     }
 
     @Override
