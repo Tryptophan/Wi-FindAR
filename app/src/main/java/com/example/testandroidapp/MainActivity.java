3432f8878e4d8e0c4af00881d2e880a8f502970c
Jacob Greenway <jagreenway12@gmail.com>
2019-02-23 22:56:37 -0600

    drop pin on map

--- app/src/main/java/com/example/testandroidapp/MainActivity.java
+++ app/src/main/java/com/example/testandroidapp/MainActivity.java
@@ -7,23 +7,41 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
 
     private SupportMapFragment mapFragment;
+
+    private FirebaseFirestore db;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Initialize firebase app
+        // Initialize firebase app
+        FirebaseApp.initializeApp(this);
+
+        // Get a reference to the firebase db
+        this.db = FirebaseFirestore.getInstance();
 
         // Add map fragment
         this.mapFragment = new SupportMapFragment();
@@ -47,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Fragment fragment = null;
         switch (view.getId()) {
             case R.id.navigation_map:
-                fragment =this.mapFragment;
+                fragment = this.mapFragment;
                 break;
             // TODO: AR fragment case
         }
@@ -64,6 +82,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
 
+        // Get router locations from firebase and add pins to map
+        CollectionReference routers = db.collection("routers");
+        routers.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot snap) {
+                ArrayList<GeoPoint> locations = new ArrayList();
+                for (QueryDocumentSnapshot router : snap) {
+                    GeoPoint location = (GeoPoint) router.get("location");
+                    LatLng marker = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.addMarker(new MarkerOptions().position(marker).title((String) router.get("ssid")));
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15));
+                }
+            }
+        });
     }
 }
